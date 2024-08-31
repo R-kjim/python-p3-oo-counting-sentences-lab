18a1ba4478a0ba5d93b0a31a94a403c8821457cc
R-kjim <rkayjim@gmail.com>
2024-08-31 21:38:02 +0300

    instance methods in python

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -2,3 +2,36 @@
 
 class MyString:
   pass
+  def __init__(self,value="nm"):
+    self.value=value
+
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self,value):
+    if(type(value)==str):
+      self._value=value
+    else:
+      print("The value must be a string.")
+  
+  def is_sentence(self):
+    return self.value.endswith(".")
+
+  def is_question(self):
+    return self.value.endswith("?")
+
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def count_sentences(self):
+    pass
+    w=self.value.replace("?",".")
+    z=w.replace("!",".")
+    print(z.split(". "))
+    if(". " in z):
+      return len(z.split(". "))
+    else:
+      return 0
+
